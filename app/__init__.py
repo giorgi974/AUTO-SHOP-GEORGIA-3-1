24aeb8ece89af66adc30eaa7c5d782fa9865e61a
giorgi974 <giorgi.loriate05@geolab.edu.ge>
2025-07-24 15:34:53 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,17 +7,23 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret-key'
+    app.config['SECRET_KEY'] = 'secret-key-აქ-შენი-საკუთარი'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
-    from app.models import User 
+    from app.models import User, Car
 
-    from app.routes import main
-    from app.auth import auth
-    app.register_blueprint(main)
-    app.register_blueprint(auth)
+    from app.auth import auth as auth_bp
+    from app.main import main as main_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
     return app
