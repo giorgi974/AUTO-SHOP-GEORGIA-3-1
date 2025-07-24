d1308c6f7a187aaf39a3c420006bb5ab290bf897
giorgi974 <giorgi.loriate05@geolab.edu.ge>
2025-07-24 15:21:45 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,23 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'supersecretkey'
+    app.config['SECRET_KEY'] = 'secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
     db.init_app(app)
     login_manager.init_app(app)
 
-    from app.models import User
+    from app.models import User  # აუცილებელია იმპორტი აქ
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
+    # რაუტების რეგისტრაცია
     from app.routes import main
     from app.auth import auth
-
     app.register_blueprint(main)
     app.register_blueprint(auth)
 
