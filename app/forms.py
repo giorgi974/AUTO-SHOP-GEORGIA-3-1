5942f4d924f837e20594e40f04e6921a94851503
giorgi974 <giorgi.loriate05@geolab.edu.ge>
2025-07-24 15:35:25 +0400

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FloatField
-from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
-from app.models import User  
+from wtforms import StringField, PasswordField, SubmitField, FloatField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError, URL, NumberRange
+from app.models import User
 
-class RegisterForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('ეს სახელი უკვე დაკავებულია, აირჩიეთ სხვა.')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -15,7 +20,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class CarForm(FlaskForm):
-    image_url = StringField('Image URL', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired(), Length(min=5)])
-    price = FloatField('Price', validators=[DataRequired()])
-    submit = SubmitField('Add Car')
+    image_url = StringField('სურათის ბმული', validators=[DataRequired(), URL()])
+    description = TextAreaField('აღწერა', validators=[DataRequired()])
+    price = FloatField('ფასი', validators=[DataRequired(), NumberRange(min=0)])
+    submit = SubmitField('დამატება')
